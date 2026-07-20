38f0e93f2187e383388bdf10dd77adff1a1925d2
michaelleechoicetech <m.lee@choicetech.kr>
2026-07-20 12:15:28 +0900

    Fix binaryTarget url (literal ${VERSION} -> v1.0.4) and pin release v1.0.4

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CTKCHPSDK",
-            url: "https://github.com/michaelleechoicetech/CTKCHPSDK-iOS/releases/download/${VERSION}/CTKCHPSDK.xcframework.zip",
+            url: "https://github.com/michaelleechoicetech/CTKCHPSDK-iOS/releases/download/v1.0.4/CTKCHPSDK.xcframework.zip",
             checksum: "a4a3aaf48a3ccfc054d91b249bac9d83b7d041ecd1ad4a9d8a05698248bf7c73"
         )
     ]
